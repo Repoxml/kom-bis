cd8d9dafa8ffef6c73b0627afcc4f242aede7204
Michał <repoxml2@gmail.com>
2026-05-17 19:18:14 +0200

    add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col min-h-screen">
       <Navbar />
+      <Hero />
       <Footer />
+      
     </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero(){
+    return (
+        <section className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white text-center">
+            <h1 className="text-5xl font-bold">Usługi komunalne w Trójmieście</h1>
+            <h3 className="text-xl mt-4">Wywóz gruzu, odśnieżanie, sprzątanie terenów. Działamy od 2009 roku.</h3>
+            <button className="mt-8 bg-yellow-400 text-black px-6 py-3 rounded-lg font-semibold">Zadzwoń: 58 341 82 59</button>
+        </section>
+    )
+}
