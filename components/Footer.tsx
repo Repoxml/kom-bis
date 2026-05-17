f06beef57dfcbe7565adcfdabd4479de83e92c4e
Michał <repoxml2@gmail.com>
2026-05-17 17:31:34 +0200

    add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-between bg-gray-900 text-white py-4 px-8 items-center">
+            <ul className="flex gap-8">
+          <li><a>Adres: Uczniowska 50, 80-530 Gdańsk</a></li>
+          <li><a href="tel:+48583418259">Tel: 58 341 82 59</a></li>
+          <li><a href="mailto:biuro@kom-bis.com">Email: biuro@kom-bis.com</a></li>
+          
+        </ul>
+        <p>© 2025 KOM-BIS Sp. z o.o.</p>
+        </footer>
+    )
+}
